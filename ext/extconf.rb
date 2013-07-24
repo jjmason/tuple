f9924f0381037a38647239f6e6e9a92c88e68fb7
jjmason <jonathan.j.mason@gmail.com>
2013-07-23 23:16:53 -0500

    So now I understand what the RUBY_1_9_x flag does...

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,10 +3,8 @@ require 'mkmf'
 case RUBY_VERSION
 when /\A1\.8/
   $CFLAGS += ' -DRUBY_1_8_x'
-when /\A1\.9/
-  $CFLAGS += ' -DRUBY_1_9_x'
-when /\A2\.0/
-  $CFLAGS += ' -DRUBY_2_0_x'  
+when /\A[12]\.9/
+  $CFLAGS += ' -DRUBY_1_9_x' 
 else
   raise "unsupported Ruby version: #{RUBY_VERSION}"
 end
