5618189b5c4884a916da3d838f7ea9f338b41c1d
jjmason <jonathan.j.mason@gmail.com>
2013-07-23 23:12:51 -0500

    Update extconf.rb to support Ruby 2.0

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,6 +5,8 @@ when /\A1\.8/
   $CFLAGS += ' -DRUBY_1_8_x'
 when /\A1\.9/
   $CFLAGS += ' -DRUBY_1_9_x'
+when /\A2\.0/
+  $CFLAGS += ' -DRUBY_2_0_x'  
 else
   raise "unsupported Ruby version: #{RUBY_VERSION}"
 end
